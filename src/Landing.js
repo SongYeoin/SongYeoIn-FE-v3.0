853f21518b784a35b2074177c8f38ba93d81a2bf
RACHEL-2929 <sts06154@naver.com>
2024-11-06 00:28:19 +0900

    🎨design: 회원가입 페이지와 로그인 페이지 생성 기존에 App.js 적혀있던 랜딩페이지를 Landing.js로 옮겼으며 라우터로 /login /join 으로 이동가능하도록 링크 걸어두었습니다. css도 기존에 따로 파일을 작성했던 것과 달리 TailWind로 작성했으며, 사용방식과 설치 방식은 노션에 남겨놓겠습니다. 작은 이슈로는 Landing.js에서 사진을 불러오는 과정에서 문제가 있어 사진이 뜨지 않습니다.

---
+++ src/Landing.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Landing = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/login'); // Navigates to the login page
+  };
+
+  return (
+    /* 전체 크기 */
+    <div
+      className="w-full h-full left-0 top-0 absolute overflow-hidden bg-white flex justify-center flex-col items-center">
+
+      {/* 배경 이미지 */}
+      <div
+        className="w-full h-full absolute bg-no-repeat bg-center bg-cover left-0 top-0 object-cover bg-[url('./images/background_jpg.jpg')]"
+      />
+
+      {/* 송파 로고와 송여인 로고 담는 div */}
+      <div
+        className="relative flex flex-row items-center gap-5 mt-12"
+      >
+        <img
+          src="/src/images/woman_up_logo.jpg"
+          className="w-[255px] h-[120px] object-cover"
+          alt="송파로고"
+        />
+
+        <svg
+          width="52"
+          height="54"
+          viewBox="0 0 52 54"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[52px] h-[54px]"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M39 13.5L13 40.5M13 13.5L39 40.5"
+            stroke="#F5F5F5"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+        <p
+          className="w-[260px] h-[81px] text-[50px] text-center text-[#1e2d1f] font-normal "
+          style={{ fontFamily: 'Dokdo' }}>
+          SONGYEOIN
+        </p>
+      </div>
+
+      {/* 소개 글 */}
+      <div className="relative flex flex-col items-center gap-5 mb-14">
+        <p
+          className="w-[567px] h-[71px] text-[39px] font-bold text-center text-[#fef5f5]"
+        >
+          COLLABORATION
+        </p>
+        <p
+          className="w-[712px] h-[63px] text-[25px] text-center text-white"
+        >
+          <span className="w-[712px] h-[63px] text-[25px] text-center text-white"
+          >송파여성인력개발센터와 ‘자바 스프링 백엔드’ 훈련 과정 수료생</span>
+          <br />
+          <span className="w-[712px] h-[63px] text-[25px] text-center text-white"
+          >‘송여인’ 팀 5인이 함께한 학습 관리 시스템입니다.</span>
+        </p>
+      </div>
+
+
+      <div className="flex flex-row items-center gap-28">
+        {/* 수강생 아이콘 div */}
+        <div
+          className="w-[502px] h-[498px] relative rounded-[80px] bg-[#fffcfc]/30 flex flex-col items-center justify-center
+          hover:bg-white/50 hover:scale-105 cursor-pointer"
+          onClick={handleClick}>
+          <img
+            src="/images/student_icon.jpg"
+            className="w-[225px] h-[242px] object-cover"
+          />
+          <p
+            className="w-[117px] h-[49px] text-4xl font-bold text-center text-[#1e2d1f]"
+          >
+            수강생
+          </p>
+        </div>
+
+        {/* 관리자 아이콘 div */}
+        <div
+          className="w-[502px] h-[500px] relative rounded-[80px] bg-[#fffcfc]/30  flex flex-col items-center justify-center
+          hover:bg-white/50 hover:scale-105 cursor-pointer"
+          onClick={handleClick}>
+          <img
+            src="./images/admin_icon.jpg"
+            className="w-[200px] h-52 object-cover"
+          />
+          <p
+            className="w-[113px] h-[49px] text-4xl font-bold text-center text-[#1e2d1f]"
+          >
+            관리자
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+};
